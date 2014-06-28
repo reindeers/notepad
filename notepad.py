885ed9af136113579a722f1187d7dbe574ce7d05
reindeers <velokarri@mail.ru>
2014-06-29 01:09:30 +0400

    add delete and edit

--- notepad.py
+++ notepad.py
@@ -1,17 +1,29 @@
+#maybe use db instead of multiple files
+import os
+
 class Note:
 	def __init__(self, data, name=None, text=None):
 		self.data = data
 		self.name = name
 		self.text = text
+
 		
 	def saveNote(self):
 		f = open(self.data +'.txt', 'w')
-		f.write(self.name + self.text )
+		f.write(self.name + ' ' + self.text )
 		f.close()
-	def deleteNewNote(self):
-		pass
-	def editNote(self): #reload data
-		pass
+
+	def deleteNote(self, data):
+		os.remove(data + '.txt')
+
+	def editNote(self, data, name=None, text=None): 
+		self.data = data
+		if name != None:
+			self.name = name
+		if text != None:	
+			self.text = text
+		self.saveNote()
+
 	def readNote(self, data):
 		f = open(data + '.txt') 
 		return f.read() 
@@ -19,4 +31,5 @@ class Note:
 
 newNote = Note('20140729', 'test', 'ttttt' )
 newNote.saveNote()
+newNote.editNote('20140729', text='lolo')
 print(newNote.readNote('20140729'))
